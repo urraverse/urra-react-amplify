da65ab4f2219c16f07e669ef9ff857ffc231e2ee
urraverse <79879359+urraverse@users.noreply.github.com>
2023-08-26 10:51:15 -0500

    Delete amplify directory

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++
@@ -1,9 +0,0 @@
-export type AmplifyDependentResourcesAttributes = {
-  "api": {
-    "urrafinancialweb10": {
-      "GraphQLAPIEndpointOutput": "string",
-      "GraphQLAPIIdOutput": "string",
-      "GraphQLAPIKeyOutput": "string"
-    }
-  }
-}
